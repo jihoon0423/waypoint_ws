f3519a8d29693e7bbb0b5cce37bae1f743e05d3c
Choi <c990305@naver.com>
2025-06-01 18:00:19 +0900

    feat:rename

--- build/waypoint_nav_pkg/prefix_override/sitecustomize.py
+++ build/waypoint_nav_pkg/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/choi/sim_ws/install/waypoint_nav_pkg'
+    sys.prefix = sys.exec_prefix = '/home/choi/nav2/install/waypoint_nav_pkg'
